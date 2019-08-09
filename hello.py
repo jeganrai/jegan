e5eb7a4ff0c23da98fbb7371c14a6fc8de711eeb
jeganrai <jeganrai10@gmail.com>
2019-08-09 16:35:46 +0530

    jeganann

--- hello.py
+++ hello.py
@@ -4,6 +4,9 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello World!kjhkjhhkj'
+@app.route('/about')
+def hello_world():
+    return 'Hello about'
 
 if __name__ == '__main__':
     app.run()
